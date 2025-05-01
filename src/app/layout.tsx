eaec8ec333a6a9dda5750dfa5b286cb67b0db4f8
Brandon C <brandonc@heyitsmass.dev>
2025-05-01 12:44:46 -0700

    adds speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { sourceCodePro } from '@/fonts';
 import metadata from '@/metadata';
 import { BoxesIcon, CircleUserRoundIcon, HouseIcon, LucideProps, PickaxeIcon, type LucideIcon } from 'lucide-react';
@@ -48,16 +48,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" id="__next" className={sourceCodePro.className}>
-			<body className="grid grid-rows-[auto_1fr] max-w-6xl w-full place-self-center justify-items-center gap-4 h-full">
-				<header className="bg-zinc-800 border-zinc-700 shadow-md max-w-6xl w-full h-12 border border-solid rounded-xl flex items-center px-8 gap-12 *:no-underline">
-					{links.map((link) => (
-						<IconLink key={link.text} {...link} size={16} />
-					))}
-				</header>
-				<main className="flex h-full w-full justify-center">{children}</main>
-			</body>
-		</html>
+		<>
+			<SpeedInsights />
+			<html lang="en" id="__next" className={sourceCodePro.className}>
+				<body className="grid grid-rows-[auto_1fr] max-w-6xl w-full place-self-center justify-items-center gap-4 h-full">
+					<header className="bg-zinc-800 border-zinc-700 shadow-md max-w-6xl w-full h-12 border border-solid rounded-xl flex items-center px-8 gap-12 *:no-underline">
+						{links.map((link) => (
+							<IconLink key={link.text} {...link} size={16} />
+						))}
+					</header>
+					<main className="flex h-full w-full justify-center">{children}</main>
+				</body>
+			</html>
+		</>
 	);
 }
 
